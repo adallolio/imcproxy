463e3dcdeffd37591a16b8e402b7cfb980581a94
adallolio <albydallo@gmail.com>
2020-04-17 16:46:31 +0000

    UTC time restored

--- src/pt/lsts/imc/ImcProxyPlot.java
+++ src/pt/lsts/imc/ImcProxyPlot.java
@@ -186,7 +186,7 @@ public class ImcProxyPlot extends ImcClientSocket {
 		} else if(temp.equals("GpsFix") && message.getSrc() == AutoNautL2)
 		{
 			console("GpsFix received");
-			PlotGpsFix.plot(message); // 4 entities: 2 from L2 and 2 from L3.
+			PlotGpsFix.plot(message);
 		}
 
 	}
